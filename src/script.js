7be7d82420fedaecd778989b977adefccb0c6ac7
Hugo Ernesto <mail.hbler@gmail.com>
2022-05-12 00:06:55 -0300

    update: task done

--- src/script.js
+++ src/script.js
@@ -1,9 +1,32 @@
 //// Imports
 import Layout from "./controllers/Layout.js";
 
+//// Functions
+function url_redirect(url) {
+  var X = setTimeout(function () {
+    window.location.replace(url);
+    return true;
+  }, 300);
+
+  if ((window.location = url)) {
+    clearTimeout(X);
+    return true;
+  } else {
+    if ((window.location.href = url)) {
+      clearTimeout(X);
+      return true;
+    } else {
+      clearTimeout(X);
+      window.location.replace(url);
+      return true;
+    }
+  }
+  return false;
+}
+
 //// Flow
 if (String(self.location.href).includes("index")) {
-  window.location = "./src/pages/login.html";
+  url_redirect("./src/pages/login.html");
 } else if (String(self.location.href).includes("login")) {
   Layout.loginPage();
 
